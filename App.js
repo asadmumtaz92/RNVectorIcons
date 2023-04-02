bb42f5492f77bbaacbff213539be001b0e010d27
Asad Mumtaz <asadmumtaz92@gmail.com>
2023-04-02 07:23:27 +0500

    This commit fot add Vector Icons for android platform...

--- App.js
+++ App.js
@@ -1,26 +1,41 @@
 import React from 'react'
 import {
     SafeAreaView,
-    StatusBar,
     StyleSheet,
-    Text,
     View,
 } from 'react-native'
+
+import AntDesign from 'react-native-vector-icons/AntDesign'
+import Icon from 'react-native-vector-icons/EvilIcons'
 
 const App = () => {
 
     return (
         <SafeAreaView style={{}}>
-            <StatusBar barStyle={'dark-content'} backgroundColor={'red'} />
 
-            <Text style={styles.highlight}>App.js</Text>
+            <View style={styles.iconBox}>
+                <Icon
+                    name='user'
+                    style={{ color: '#000', fontSize: 40 }}
+                />
+                <AntDesign
+                    name='customerservice'
+                    style={{ color: '#000', fontSize: 30 }}
+                />
+            </View>
 
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    // 
+    iconBox: {
+        justifyContent: 'space-evenly',
+        borderBottomWidth: 0.5,
+        flexDirection: 'row',
+        marginVertical: 10,
+        paddingBottom: 10,
+    }
 });
 
 export default App
